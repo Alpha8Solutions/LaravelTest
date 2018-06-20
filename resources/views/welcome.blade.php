321a8ba5ac66ed0cd63f032f965886010bed9a6c
Anthony Makanju <Anthony.Makanju@alltran.com>
2018-06-20 14:12:22 -0500

    more design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,9 @@
 <html>
     <head>
       <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
       <title>Incollab | We build what you dream</title>
       <link href="{{ asset('css/gradients.css') }}" rel="stylesheet" type="text/css">
       <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css">
